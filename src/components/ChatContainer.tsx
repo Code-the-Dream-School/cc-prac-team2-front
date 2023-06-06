0c4debeced700f0efc34251b31bede1b5bd4c2c8
Said-laasri <saidlaasri3@gmail.com>
2023-06-06 17:11:37 +0100

    Add light and Dark mode

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,120 +1,136 @@
-
-import {useState, useContext, useEffect, useRef} from 'react'
+import { useState, useContext, useEffect, useRef } from "react";
 import { toast } from "react-toastify";
-import axios from "axios"
-import ChatInput from "../components/ChatInput"
-import {UserContext} from "../context/user-context"
-
+import axios from "axios";
+import ChatInput from "../components/ChatInput";
+import { UserContext } from "../context/user-context";
 
 interface Messages {
-    _id: string,
-    sender: string
-    message: string,
+  _id: string;
+  sender: string;
+  message: string;
 }
 
 const ChatContainer = (): JSX.Element => {
-    const {
-        user, setUser, 
-        conversation, setConversation, 
-        selectId, setSelectId
-    } = useContext(UserContext)
+  const {
+    user,
+    setUser,
+    conversation,
+    setConversation,
+    selectId,
+    setSelectId,
+    isDarkMode,
+  } = useContext(UserContext);
 
-    const [messages, setMessages] = useState<Messages[]>([])
- 
-    const scrollRef = useRef<HTMLDivElement | null>(null)
-    const token: {token: string } | null = JSON.parse(localStorage.getItem("token") || "null")
+  const [messages, setMessages] = useState<Messages[]>([]);
 
-console.log(user);
-// console.log(selectId);
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const token: { token: string } | null = JSON.parse(
+    localStorage.getItem("token") || "null"
+  );
 
+  const sendMessage = async (messageText: any) => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8000/api/v1/messages",
+        {
+          from: user?.userId,
+          to: selectId,
+          message: messageText,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setMessages((prev) => [
+        ...prev,
+        {
+          from: user.userId,
+          to: selectId,
+          message: messageText,
+        },
+      ]);
+    } catch (err) {
+      console.log(err);
+      toast.error("Error sending messages, please try again");
+    }
+  };
 
-    const sendMessage = async (messageText:any) => {
-        try {
-            const {data} = await axios.post('http://localhost:8000/api/v1/messages', {
-                from: user?.userId,
-                to: selectId, 
-                message: messageText
+  const fetchMessages = async () => {
+    try {
+      if (conversation) {
+        const { data } = await axios.get(
+          `http://localhost:8000/api/v1/users/${user.userId}/conversations/${conversation}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
             },
-            {
-                headers: {
-                  Authorization: `Bearer ${token}`
-                }
-              }
-            )
-            setMessages( prev => [...prev, {
-                from: user.userId,
-                to: selectId, 
-                message: messageText
-            }])
-        } catch (err) {
-            console.log(err);
-            toast.error("Error sending messages, please try again");
-        }
+          }
+        );
+        const { messages } = data.conversation;
+        setMessages(messages);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("Error fetching messages, please try again");
     }
+  };
 
-    console.log(messages);
-    
+  useEffect(() => {
+    fetchMessages();
+  }, [conversation]);
 
-    const fetchMessages = async () => {
-        try {
-            if (conversation) {
-                const {data} = await axios.get(`http://localhost:8000/api/v1/users/${user.userId}/conversations/${conversation}`, 
-                {
-                    headers: {
-                      Authorization: `Bearer ${token}`
-                    }
-                  }
-                )
-                const {messages} = data.conversation
-                setMessages(messages)
-            }
-        } catch (err) {
-            console.log(err);
-            toast.error("Error fetching messages, please try again");
-        }
-    }
-
-    useEffect(() => {
-        fetchMessages()
-    }, [conversation])
-
-    
-
-    return (
-        <>
-
-            <div className="flex-grow h-screen bg-slate-200">
-                <div className="w-full h-5/6 bg-blue-100" > 
-                <div className="relative h-full">
-                <div className="overflow-y-scroll absolute top-0 left-0 right-0 bottom-2"> 
-                <div className=''>
-                    {messages ? messages.map((msg) => (
-
-                        <div 
-                        className={(msg.sender === user?.userId ? 'text-right' : 'text-left')}
+  return (
+    <>
+      <div
+        className={`flex-grow h-screen ${
+          isDarkMode ? "bg-gray-900" : "bg-gray-100"
+        }`}
+      >
+        <div
+          className={`w-full h-5/6 ${
+            isDarkMode ? "bg-gray-800" : "bg-gray-200"
+          }`}
+        >
+          <div className="relative h-full">
+            <div className="overflow-y-scroll absolute top-0 left-0 right-0 bottom-2">
+              <div className="">
+                {messages
+                  ? messages.map((msg) => (
+                      <div
+                        className={
+                          msg.sender === user?.userId
+                            ? "text-right"
+                            : "text-left"
+                        }
                         key={msg._id}
+                      >
+                        <div
+                          className={`max-w-md text-left inline-block rounded-lg ${
+                            isDarkMode ? "bg-gray-500 text-white" : "bg-white"
+                          } m-2 p-2`}
                         >
-                        <div className={('max-w-md text-left inline-block rounded-lg bg-white m-2 p-2 ' + (msg.sender === user?.userId ? 'bg-slate-500' : null) )}>{msg.message}</div>
+                          {msg.message}
                         </div>
-                    )) : null}
-                </div>
-                <div ref={scrollRef}></div>
-                </div>
-                </div> 
-                </div>
-                <div className="w-full h-1/6 bg-slate-300" >   
-                <ChatInput onHandleSendMessage={sendMessage} />
-                </div>
-  
+                      </div>
+                    ))
+                  : null}
+              </div>
+              <div ref={scrollRef}></div>
             </div>
-            
-        
-        </>
-    )
-}
+          </div>
+        </div>
+        <div
+          className={`w-full h-1/6 ${
+            isDarkMode ? "bg-gray-700" : "bg-gray-300"
+          }`}
+        >
+          <ChatInput onHandleSendMessage={sendMessage} />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ChatContainer
-
-
-
-
+export default ChatContainer;
