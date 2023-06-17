18ccba21628675c1d4677ef69463fa78bbca36bc
Minh Tam Pham <miminiverse@Minhs-MacBook-Pro.local>
2023-06-16 21:10:13 -0400

    sending translated text real time

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -104,17 +104,18 @@ const ChatContainer = ({ socket }: { socket: Socket }): JSX.Element => {
               }
             )
             const {message} = data
+            console.log(message)
 
             socket.current.emit("sendMessage", {
                 createdAt: message.createdAt,
                 from: user?._id,
                 to: selectId, 
-                message: messageText
+                message: message.message
             })
 
             setMessages( prev => [...prev, {
                 createdAt: message.createdAt,
-                message: messageText, 
+                message: message.message, 
                 sender: user?._id, 
                 _id: message._id,
             }])
@@ -214,11 +215,7 @@ const ChatContainer = ({ socket }: { socket: Socket }): JSX.Element => {
 
                         {msg.message && msg.message.includes('\n') ? (
                 msg.message.split('\n').map((line, index, lines) => {
-                  console.log(lines[0]);
-                  console.log(lines[1])
-                  console.log(lines[0] === lines[1])
                   const prevLine = index > 0 ? lines[index - 1] : null;
-                  console.log(prevLine);
                   const isFirstLine = index === 0 || line !== prevLine;
                   
                   return (
