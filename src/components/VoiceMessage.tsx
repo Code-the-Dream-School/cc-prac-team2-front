a65cc95c256fa2ecdc113ed8f135f08db2ad2cfa
Minh Tam Pham <miminiverse@Minhs-MacBook-Pro.local>
2023-06-16 22:51:00 -0400

    fix the input in chatContainer

--- src/components/VoiceMessage.tsx
+++ src/components/VoiceMessage.tsx
@@ -107,7 +107,7 @@ const VoiceMessage = ({ socket }: { socket: Socket }) => {
         <>
       <div className="w-1/3">
         <div className='flex items-center justify-center'>
-        <img  src={Wave} alt="Wave" width="30" height="80"/>
+        <img  src={Wave} alt="Wave" width="25" height="80"/>
         </div>
       </div> 
         <button
