20290d67da3420ed258f09d19b073c2a4e63be31
Roy Mosby <ego.madking@gmail.com>
2023-06-21 09:35:11 -0500

    add comments, remove invalid dep

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -2,7 +2,6 @@ import { useState, useContext, useEffect, useRef } from "react";
 import axios from "axios";
 import ChatContainer from "../components/ChatContainer";
 import { UserContext } from "../context/user-context";
-import COCKATOO from "./../assests/cockatoo.png";
 import { getContactName } from "../util/getContactName";
 import { io, Socket } from "socket.io-client";
 
@@ -13,10 +12,17 @@ type MyEventMap = {
   getUsers: (users: string[]) => void;
 };
 
+//`User` interface is already defined in user-context
+// however, it is composed differently.
+// This should be reconciled so you are not rebuilding
+// objects around the codebase, especially since
+// UserContext is present in this file
 interface User {
     _id: string;
     userName: string;
+    // how does conversation compare to `message` in `Messages` interface? If they are the same, this should be reconciled.
     conversation: string;
+    profileImage?: ProfileImage
   }
 
 interface UsersList {
@@ -24,7 +30,13 @@ interface UsersList {
     uncontactedUsers: User[];
   }
 
-const Chat = () => {
+// should end up co-located with `User`
+interface ProfileImage {
+  public_id: string,
+  url: string
+}
+
+const Chat = (): JSX.Element => {
   const {
     user,
     conversationId,
@@ -54,14 +66,17 @@ const Chat = () => {
       socket.current.emit("addUser", user._id);
       socket.current.on("getUsers", (users: unknown[]) => {
         let usersMap = new Set();
-        users.map((user:any) => {
+        //map should return something.
+        //if you need an iterator, use forEach instead
+        users.forEach((user:any) => {
           usersMap.add(user[0]);
           let usersArray:any[] = Array.from(usersMap);
           setOnlineUsers(usersArray);
         });
       });
     }
-  }, [socket.current, user]);
+    //socket.current is not a valid dep. Were you trying to accomplish something specific?
+  }, [user]);
 
 
   useEffect(() => {
@@ -80,13 +95,14 @@ const Chat = () => {
         Authorization: `Bearer ${token}`,
       },
     });
-    console.log(data);
+    console.log(data.users.contactedUsers[2]);
     setUsersList(data.users)
   };
 
 
   useEffect(() => {
     fetchUsers();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSelectContact = (u: User) => {
@@ -95,7 +111,7 @@ const Chat = () => {
   };
 
   const handleSelectUnContact = (unContact:User) => {
-    setConversationId(null);
+    setConversationId(null); // why null?
     setSelectId(unContact._id);
   };
 
@@ -112,6 +128,9 @@ const Chat = () => {
           >
             Contact
           </div>
+          {/* Consider decomposing this into sub-component since we are returning 
+            some JSX per user in the contactedUsers. It'll simplify this component 
+            and we will also be able to re-use it for the uncontactedUsers below. */}
           {usersList ? usersList.contactedUsers.map((u) => {
             return (
                 <div key={u._id}
@@ -129,6 +148,7 @@ const Chat = () => {
                     <img
                     className="w-12 h-10 rounded-full shadow-xl"
                     src={u.profileImage?.url}
+                    alt=""
                     />
                     {getContactName(u.userName, onlineFriends )}
                   </div>
@@ -148,13 +168,13 @@ const Chat = () => {
             People
           </div>
           <div>
+            {/* Consider breaking this up into a sub-component. See comment above*/}
             {usersList
               ? usersList.uncontactedUsers.map((unContact) => {
                   if (unContact._id === user?._id) {
                     return null;
                   }
                   return (
-           
                       <div key={unContact._id}
                         className={
                           "flex bg-slate-300 rounded-lg m-3 p-2 cursor-pointer " +
@@ -162,22 +182,22 @@ const Chat = () => {
                         }
                         onClick={() => handleSelectUnContact(unContact)}
                       >
-                         <div className="flex flex-row gap-4">
-                <div className="h-full w-1/3 items-center justify-between">
-                  <div className="relative">
-                    <img
-                    className="w-12 h-12 rounded-full border-2 shadow-sm"
-                    src={unContact.profileImage?.url}
-                    />
-                    {getContactName(unContact.userName, onlineFriends )}
-                  </div>
-                  </div>
-                  <div className="h-full w-2/3 items-center justify-center">
-                    <div className="m-auto text-center items-center justify-center">{unContact.userName}</div>
-                  </div>
-                </div>
+                        <div className="flex flex-row gap-4">
+                        <div className="h-full w-1/3 items-center justify-between">
+                          <div className="relative">
+                            <img
+                            className="w-12 h-12 rounded-full border-2 shadow-sm"
+                            src={unContact.profileImage?.url}
+                            alt=""
+                            />
+                            {getContactName(unContact.userName, onlineFriends )}
+                          </div>
+                          </div>
+                          <div className="h-full w-2/3 items-center justify-center">
+                            <div className="m-auto text-center items-center justify-center">{unContact.userName}</div>
+                          </div>
+                        </div>
                       </div>
-                 
                   );
                 })
               : null}
