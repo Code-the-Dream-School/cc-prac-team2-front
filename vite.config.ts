2512091bdc3623cf13e0f3f66c2f04f3ff844ed3
Roy Mosby <ego.madking@gmail.com>
2023-06-20 19:47:56 -0500

    add eslint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 
-
+/** @type {import('vite').UserConfig} */
 export default defineConfig(() => {
   return {
     build: {
       outDir: 'build',
     },
-    plugins: [react()],
+    plugins: [react(), eslint()],
   };
 });
